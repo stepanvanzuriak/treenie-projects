b40541ad0bc75e803a377129de975b44629b3913
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-07-09 17:50:20 +0300

    Fix async test

--- redux-thunk-api/src/actions/actions.test.js
+++ redux-thunk-api/src/actions/actions.test.js
@@ -2,12 +2,76 @@ import { setBooks, addError, nextPage, prevPage, getBooks } from './actions'
 import { SET_BOOKS, ADD_ERROR, NEXT_PAGE, PREV_PAGE } from './actionTypes'
 
 import configureMockStore from 'redux-mock-store'
+import moxios from 'moxios'
 import thunk from 'redux-thunk'
 import moment from 'moment'
 import { chunks } from '../utils/utils'
 import { book } from '../api/books'
+import { instance } from '../api/client'
 
 const mockStore = configureMockStore([thunk])
+
+describe('async actions', () => {
+  beforeEach(() => {
+    moxios.install(instance)
+  })
+  afterEach(() => {
+    moxios.uninstall(instance)
+  })
+
+  test('getBooks returns right action', async () => {
+    const data = [
+      {
+        ID: 1,
+        Title: 'Book 1',
+        Description:
+          'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\n',
+        PageCount: 100,
+        Excerpt:
+          'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\n',
+        PublishDate: '2018-07-08T14:41:49.8522461+00:00'
+      },
+      {
+        ID: 1,
+        Title: 'Book 1',
+        Description:
+          'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\n',
+        PageCount: 100,
+        Excerpt:
+          'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\nLorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.\r\n',
+        PublishDate: '2018-07-08T14:41:49.8522461+00:00'
+      }
+    ]
+
+    const formatData = book => ({
+      ...book,
+      Description: book.Description.substr(0, 100),
+      Excerpt: book.Excerpt.substr(0, 150),
+      PublishDate: moment(book.PublishDate).format('L')
+    })
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: data
+      })
+    })
+
+    const store = mockStore({})
+    const pageSize = 6
+
+    await store.dispatch(getBooks(pageSize))
+
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: SET_BOOKS,
+      payload: {
+        books: [data.map(formatData)]
+      }
+    })
+  })
+})
 
 test('setBooks returns right action', () => {
   expect(setBooks([1, 2, 3])).toEqual({
@@ -31,25 +95,4 @@ test('nextPage returns right action', () => {
 
 test('prevPage returns right action', () => {
   expect(prevPage()).toEqual({ type: PREV_PAGE })
-})
-
-test('getBooks returns right action', async () => {
-  const store = mockStore({})
-  const pageSize = 6
-
-  await store.dispatch(getBooks(pageSize))
-  let books = await book()
-
-  books = chunks(
-    books.data.map(book => ({
-      ...book,
-      Description: book.Description.substr(0, 100),
-      Excerpt: book.Excerpt.substr(0, 150),
-      PublishDate: moment(book.PublishDate).format('L')
-    })),
-    pageSize
-  )
-
-  const actions = store.getActions()
-  expect(actions[0]).toEqual({ type: SET_BOOKS, payload: { books } })
 })
