6f24016c90b0bb2a7d1cb0d6d017b18a8591c163
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-07-10 10:35:16 +0300

    Add test for formatBook

--- redux-thunk-api/src/utils/utils.test.js
+++ redux-thunk-api/src/utils/utils.test.js
@@ -1,4 +1,4 @@
-import { chunks } from './utils'
+import { chunks, formatBook } from './utils'
 
 test('Normal array dividing', () => {
   expect(chunks([1, 2, 3, 4], 2)).toEqual([[1, 2], [3, 4]])
@@ -19,4 +19,26 @@ test('Without size', () => {
   expect(chunks([1, 2, 3])).toEqual([1, 2, 3])
   expect(chunks([1, 2, 3], null)).toEqual([1, 2, 3])
   expect(chunks([1, 2, 3], undefined)).toEqual([1, 2, 3])
+})
+
+test('Normal book formating', () => {
+  expect(
+    formatBook({
+      ID: 1,
+      Title: 'Book 1',
+      Description: 'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.',
+      PageCount: 100,
+      Excerpt:
+        'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.',
+      PublishDate: '2018-07-08T14:41:49.8522461+00:00'
+    })
+  ).toEqual({
+    Description: 'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem.',
+    Excerpt:
+      'Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem. Lorem lorem lorem',
+    ID: 1,
+    PageCount: 100,
+    PublishDate: '07/08/2018',
+    Title: 'Book 1'
+  })
 })
